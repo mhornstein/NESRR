77797dc785134f11c9946bfdf2bc560b05695b72
maor hornstein <mor.h100@gmail.com>
2023-06-11 11:21:52 +0300

    Adding dataloader creation

--- part3/config.py
+++ part3/config.py
@@ -1,1 +1,4 @@
 MI_TRANSFORMATION = 'ln' # can be either None, minmax, ln, or sqrt
+
+BERT_MODEL = 'bert-base-cased'
+BATCH_SIZE = 32
--- part3/regressor.py
+++ part3/regressor.py
@@ -3,6 +3,12 @@ from config import *
 import sys
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
+from sklearn.model_selection import train_test_split
+from transformers import AutoModel, BertTokenizerFast
+import torch
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning) # Disable the warning
 
 if len(sys.argv) == 1:
     raise ValueError("Path to dataset missing")
@@ -28,7 +34,28 @@ def transform_mi(series, transformation_type):
         scaled_series = pd.Series(scaled_data.flatten())
     return scaled_series
 
+def create_data_loader(tokenizer, X, y, max_length, batch_size):
+    tokens = tokenizer.batch_encode_plus(X.tolist(), max_length=max_length, pad_to_max_length=True, truncation=True, return_token_type_ids=False)
+    seq = torch.tensor(tokens['input_ids'])
+    mask = torch.tensor(tokens['attention_mask'])
+
+    y_tensor = torch.tensor(y.tolist())
+
+    dataset = TensorDataset(seq, mask, y_tensor)
+    sampler = RandomSampler(dataset)
+    dataloader = DataLoader(dataset, sampler=sampler, batch_size=batch_size)
+
+    return dataloader
+
 if __name__ == '__main__':
     df = pd.read_csv(input_file)
     df['mi_score'] = transform_mi(df['mi_score'], MI_TRANSFORMATION)
-    print()
+    max_length = max([len(s.split()) for s in df['masked_sent']])
+
+    X_train, X_tmp, y_train, y_tmp = train_test_split(df['masked_sent'], df['mi_score'], random_state=42, test_size=0.3)
+    X_val, X_test, y_val, y_test = train_test_split(X_tmp, y_tmp, random_state=42, test_size=0.5)
+
+    tokenizer = BertTokenizerFast.from_pretrained(BERT_MODEL)
+    train_dataloader = create_data_loader(tokenizer, X_train, y_train, max_length, BATCH_SIZE)
+    validation_dataloader = create_data_loader(tokenizer, X_val, y_val, max_length, BATCH_SIZE)
+
