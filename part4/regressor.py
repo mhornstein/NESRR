6e29ad5b6881f09d7d3e0240a32d3c2e5ff0ebcb
maor hornstein <mor.h100@gmail.com>
2023-06-16 09:59:40 +0300

    Update regressor 4 to output test predictions

--- part4/regressor.py
+++ part4/regressor.py
@@ -59,7 +59,6 @@ def create_df(data_file, embs_file, mi_transformation):
     df = pd.read_csv(data_file)
     df['mi_score'] = transform_mi(df['mi_score'], mi_transformation)
 
-    df = df[['sent_id', 'mi_score']]
     df = df.set_index('sent_id')
 
     # Load embeddings file
@@ -71,11 +70,12 @@ def create_df(data_file, embs_file, mi_transformation):
 
     return df
 
-def create_data_loader(X, y, batch_size):
+def create_data_loader(X, y, batch_size, shuffle=True):
+    ids_tensor = torch.tensor(X.index, dtype=torch.int64).unsqueeze(dim=1).to(device)
     X_tensor = torch.tensor(X.values, dtype=torch.float32).to(device)
     y_tensor = torch.tensor(y.values.reshape(-1, 1), dtype=torch.float32).to(device)
-    dataset = TensorDataset(X_tensor, y_tensor)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    dataset = TensorDataset(ids_tensor, X_tensor, y_tensor)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
     return dataloader
 
 if __name__ == '__main__':
@@ -83,9 +83,11 @@ if __name__ == '__main__':
 
     df = create_df(data_file=input_file, embs_file=embeddings_file, mi_transformation=MI_TRANSFORMATION)
 
-    X_train, X_val, y_train, y_val = train_test_split(df.iloc[:, :-1], df['mi_score'], random_state=42, test_size=0.3)
-    train_dataloader = create_data_loader(X_train, y_train, BATCH_SIZE)
-    validation_dataloader = create_data_loader(X_val, y_val, BATCH_SIZE)
+    X_train, X_tmp, y_train, y_tmp = train_test_split(df.iloc[:, :BERT_OUTPUT_SHAPE], df['mi_score'], random_state=42, test_size=0.3)
+    X_val, X_test, y_val, y_test = train_test_split(X_tmp, y_tmp, random_state=42, test_size=0.5)
+    train_dataloader = create_data_loader(X_train, y_train, BATCH_SIZE, shuffle=True)
+    validation_dataloader = create_data_loader(X_val, y_val, BATCH_SIZE, shuffle=True)
+    test_dataloader = create_data_loader(X_test, y_test, BATCH_SIZE, shuffle=False)
 
     model = BERT_Regressor(input_dim=BERT_OUTPUT_SHAPE, hidden_layers_config=REGRESSION_NETWORK_HIDDEN_LAYERS_CONFIG)
     model.to(device)
@@ -102,8 +104,7 @@ if __name__ == '__main__':
         model.train()
         total_loss = 0
         for batch in train_dataloader:
-            embeddings = batch[0]
-            targets = batch[1]
+            ids, embeddings, targets = batch
 
             outputs = model(embeddings)
             loss = criterion(outputs, targets)
@@ -119,8 +120,7 @@ if __name__ == '__main__':
         val_total_loss = 0
         with torch.no_grad():
             for val_batch in validation_dataloader:
-                val_embeddings = batch[0]
-                val_targets = batch[1]
+                val_ids, val_embeddings, val_targets = val_batch
 
                 val_outputs = model(val_embeddings)
                 val_loss = criterion(val_outputs, val_targets)
@@ -138,3 +138,37 @@ if __name__ == '__main__':
         print('\n'.join(key + ': ' + str(value) for key, value in result_entry.items()) + '\n')
 
     results_to_files(results_dict=results, output_dir=OUTPUT_DIR)
+
+    print('Start testing...')
+    model.eval()
+
+    out_df = pd.DataFrame(columns=['target_mi', 'predicted_mi', 'abs_mi_err',
+                                   'label1', 'label2', 'ent1', 'ent2', 'masked_sent'])
+    test_total_loss = 0
+    with torch.no_grad():
+        for test_batch in test_dataloader:
+            test_ids, test_embeddings, test_targets = test_batch
+            test_outputs = model(test_embeddings)
+            absolute_errors = torch.abs(test_outputs - test_targets)
+            test_loss = criterion(test_outputs, test_targets)
+            test_total_loss += test_loss.item()
+
+            batch_results = pd.DataFrame({'sent_ids': test_ids.squeeze().numpy(),
+                                       'target_mi': test_targets.squeeze().numpy(),
+                                       'predicted_mi': test_outputs.squeeze().numpy(),
+                                       'abs_mi_err': absolute_errors.squeeze().numpy()})
+            batch_results = batch_results.set_index('sent_ids', drop=True)
+            batch_results.index.name = None # remove index column name
+
+            batch_data = df.loc[torch.squeeze(test_ids)]
+            batch_data = batch_data[['label1', 'label2', 'ent1', 'ent2', 'masked_sent']]
+
+            batch_df = pd.concat([batch_results, batch_data], axis=1)
+
+            out_df = out_df.append(batch_df, ignore_index=False)
+
+    avg_test_loss = test_total_loss / len(test_dataloader)
+
+    out_df.to_csv(f'{OUTPUT_DIR}/test_predictions_results.csv', index=True)
+    with open(f'{OUTPUT_DIR}/test_loss.txt', 'w') as file:
+        file.write(f'Average test loss: {avg_test_loss}')
