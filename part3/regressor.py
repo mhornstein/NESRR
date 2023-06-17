9e5b1d306065c1afeb5bb2ccd900dc2cc983dc14
maor hornstein <mor.h100@gmail.com>
2023-06-17 10:54:23 +0300

    Update regressor 3 to output test predictions

--- part3/regressor.py
+++ part3/regressor.py
@@ -38,15 +38,16 @@ def create_df(data_file, mi_transformation):
     df = df.set_index('sent_id')
     return df
 
-def create_data_loader(tokenizer, X, y, max_length, batch_size):
+def create_data_loader(tokenizer, X, y, max_length, batch_size, shuffle):
     tokens = tokenizer.batch_encode_plus(X.tolist(), max_length=max_length, pad_to_max_length=True, truncation=True, return_tensors='pt')
 
-    ids = tokens['input_ids']
-    mask = tokens['attention_mask']
+    sent_ids = torch.tensor(X.index, dtype=torch.int64).unsqueeze(dim=1).to(device)
+    ids = tokens['input_ids'].to(device)
+    mask = tokens['attention_mask'].to(device)
     y_tensor = torch.tensor(y.values.reshape(-1, 1), dtype=torch.float32).to(device)
 
-    dataset = TensorDataset(ids, mask, y_tensor)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    dataset = TensorDataset(sent_ids, ids, mask, y_tensor)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
 
     return dataloader
 
@@ -55,12 +56,14 @@ if __name__ == '__main__':
 
     # Preparing the data
     df = create_df(input_file, MI_TRANSFORMATION)
-    X_train, X_val, y_train, y_val = train_test_split(df['masked_sent'], df['mi_score'], random_state=42, test_size=0.3)
+    X_train, X_tmp, y_train, y_tmp = train_test_split(df['masked_sent'], df['mi_score'], random_state=42, test_size=0.3)
+    X_val, X_test, y_val, y_test = train_test_split(X_tmp, y_tmp, random_state=42, test_size=0.5)
 
     tokenizer = BertTokenizerFast.from_pretrained(BERT_MODEL)
     max_length = max([len(s.split()) for s in df['masked_sent']])
-    train_dataloader = create_data_loader(tokenizer, X_train, y_train, max_length, BATCH_SIZE)
-    validation_dataloader = create_data_loader(tokenizer, X_val, y_val, max_length, BATCH_SIZE)
+    train_dataloader = create_data_loader(tokenizer, X_train, y_train, max_length, BATCH_SIZE, shuffle=True)
+    validation_dataloader = create_data_loader(tokenizer, X_val, y_val, max_length, BATCH_SIZE, shuffle=True)
+    test_dataloader = create_data_loader(tokenizer, X_test, y_test, max_length, BATCH_SIZE, shuffle=False)
 
     # Preparing the model
     config = BertConfig.from_pretrained(BERT_MODEL, num_labels=1)  # Set num_labels=1 for regression
@@ -75,11 +78,7 @@ if __name__ == '__main__':
         start_time = time.time()
         model.train()
         total_loss = 0
-        for batch in train_dataloader:
-            input_ids = batch[0].to(device)
-            attention_mask = batch[1].to(device)
-            targets = batch[2].to(device)
-
+        for sent_ids, input_ids, attention_mask, targets in train_dataloader:
             outputs = model(input_ids, attention_mask=attention_mask, labels=targets)
             loss = outputs.loss
             total_loss += loss.item()
@@ -93,11 +92,7 @@ if __name__ == '__main__':
         model.eval()
         val_total_loss = 0
         with torch.no_grad():
-            for val_batch in validation_dataloader:
-                val_input_ids = val_batch[0].to(device)
-                val_attention_mask = val_batch[1].to(device)
-                val_targets = val_batch[2].to(device)
-
+            for val_sent_ids, val_input_ids, val_attention_mask, val_targets in validation_dataloader:
                 val_outputs = model(val_input_ids, attention_mask=val_attention_mask, labels=val_targets)
                 val_total_loss += val_outputs.loss.item()
 
@@ -112,3 +107,36 @@ if __name__ == '__main__':
         print('\n'.join(key + ': ' + str(value) for key, value in result_entry.items()) + '\n')
 
     results_to_files(results_dict=results, output_dir=OUTPUT_DIR)
+
+    print('Start testing...')
+
+    model.eval()
+
+    out_df = pd.DataFrame(columns=['target_mi', 'predicted_mi', 'abs_mi_err',
+                                   'ent1', 'label1', 'ent2', 'label2', 'masked_sent'])
+    test_total_loss = 0
+    with torch.no_grad():
+        for test_sent_ids, test_input_ids, test_attention_mask, test_targets in test_dataloader:
+            test_outputs = model(test_input_ids, attention_mask=test_attention_mask, labels=test_targets)
+            absolute_errors = torch.abs(test_outputs.logits - test_targets)
+            test_total_loss += test_outputs.loss.item()
+
+            batch_results = pd.DataFrame({'sent_ids': test_sent_ids.squeeze().numpy(),
+                                          'target_mi': test_targets.squeeze().numpy(),
+                                          'predicted_mi': test_outputs.logits.squeeze().numpy(),
+                                          'abs_mi_err': absolute_errors.squeeze().numpy()})
+            batch_results = batch_results.set_index('sent_ids', drop=True)
+            batch_results.index.name = None  # remove index column name
+
+            batch_data = df.loc[torch.squeeze(test_sent_ids)]
+            batch_data = batch_data[['label1', 'label2', 'ent1', 'ent2', 'masked_sent']]
+
+            batch_df = pd.concat([batch_results, batch_data], axis=1)
+
+            out_df = out_df.append(batch_df, ignore_index=False)
+
+    avg_test_loss = test_total_loss / len(test_dataloader)
+
+    out_df.to_csv(f'{OUTPUT_DIR}/test_predictions_results.csv', index=True)
+    with open(f'{OUTPUT_DIR}/test_report.txt', 'w') as file:
+        file.write(f'Average test loss: {avg_test_loss}')
