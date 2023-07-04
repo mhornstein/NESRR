67d1e0ff16ab80468eb4e762d6f654b1b08698af
maor hornstein <mor.h100@gmail.com>
2023-07-04 15:12:34 +0300

    Move create_network from regressor_util -> util

--- common/regressor_util.py
+++ common/regressor_util.py
@@ -3,7 +3,6 @@ This utility class provides common functions that are shared among all regressor
 '''
 
 import numpy as np
-import torch.nn as nn
 
 def transform_mi(series, transformation_type):
     '''
@@ -23,37 +22,3 @@ def transform_mi(series, transformation_type):
     else:
         raise ValueError(f'Unknown MI transformation: {transformation_type}')
     return scaled_series
-
-def create_network(input_dim, hidden_layers_config, output_dim):
-    '''
-    Creates a deep model according to the given configuration
-    :param input_dim: the input dimension of the network
-    :param hidden_layers_config: indicates the hidden layers configuration of the network. \
-                                 its format: [hidden_dim_1, dropout_rate_1, hidden_dim_2, dropout_rate_2, ...]. \
-                                 for no dropout layer, use None value.
-    :param output_dim: the output dimension of the network
-    :return: the created model
-    '''
-    layers = []
-
-    prev_dim = input_dim
-    num_layers = len(hidden_layers_config) // 2  # Each layer has a dim and dropout rate
-
-    for i in range(num_layers):
-        dim = hidden_layers_config[i * 2]
-        dropout_rate = hidden_layers_config[i * 2 + 1]
-
-        layers.append(nn.Linear(prev_dim, dim))
-
-        if dropout_rate is not None:
-            layers.append(nn.Dropout(dropout_rate))
-
-        layers.append(nn.ReLU())
-
-        prev_dim = dim
-
-    layers.append(nn.Linear(prev_dim, output_dim))
-
-    model = nn.Sequential(*layers)
-
-    return model
