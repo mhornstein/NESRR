cafde2032e72265d6da811acc48bf506eab69d59
maor hornstein <mor.h100@gmail.com>
2023-07-10 15:20:56 +0300

    nit: move logit_to_predicted_label + calc_weight to classifier util

--- 4 Model 1 - Finetune BERT/classifier.py
+++ 4 Model 1 - Finetune BERT/classifier.py
@@ -31,28 +31,6 @@ def create_data_loader(tokenizer, X, y, max_length, batch_size, shuffle):
 
     return dataloader
 
-def logit_to_predicted_label(logits):
-    labels = logits.argmax(1)
-    return labels
-
-def calc_weight(labels):
-    '''
-    Returns a tensor representing the weights for label 0 and label 1 respectively.
-    for the weights, We want the positive class weight to be inversely proportional to the proportion of positive examples.
-    If there are fewer positive examples, we increase the weight to give more importance to those examples during the loss computation.
-    We use 2 for sclaling.
-    Therefore, if N = #all-samples, P = #positive-samples: 1/ 2 * frequency = 1 / 2 * (P/N) = N / 2 * P
-    The same apply for negative samples.
-    Partial reference: https://forums.fast.ai/t/about-weighted-bceloss/78570/3
-    '''
-    total_examples = len(labels)
-    num_positive = np.sum(labels == 1)
-    num_negative = total_examples - num_positive
-
-    positive_weight = total_examples / (2 * num_positive)
-    negative_weight = total_examples / (2 * num_negative)
-    return torch.tensor([negative_weight, positive_weight], dtype=torch.float32) # dtype of float 32 is the requirement of the weight
-
 def calc_measurements(model, dataloader, criterion):
     total_loss = total_good = total = 0
     with torch.no_grad():
--- common/classifier_util.py
+++ common/classifier_util.py
@@ -15,3 +15,25 @@ def score_to_label(y_train, y_tmp, score_threshold_type, score_threshold_value):
     y_tmp = np.where(y_tmp < treshold, 0, 1)
 
     return y_train, y_tmp
+
+def logit_to_predicted_label(logits):
+    labels = logits.argmax(1)
+    return labels
+
+def calc_weight(labels):
+    '''
+    Returns a tensor representing the weights for label 0 and label 1 respectively.
+    for the weights, We want the positive class weight to be inversely proportional to the proportion of positive examples.
+    If there are fewer positive examples, we increase the weight to give more importance to those examples during the loss computation.
+    We use 2 for sclaling.
+    Therefore, if N = #all-samples, P = #positive-samples: 1/ 2 * frequency = 1 / 2 * (P/N) = N / 2 * P
+    The same apply for negative samples.
+    Partial reference: https://forums.fast.ai/t/about-weighted-bceloss/78570/3
+    '''
+    total_examples = len(labels)
+    num_positive = np.sum(labels == 1)
+    num_negative = total_examples - num_positive
+
+    positive_weight = total_examples / (2 * num_positive)
+    negative_weight = total_examples / (2 * num_negative)
+    return torch.tensor([negative_weight, positive_weight], dtype=torch.float32) # dtype of float 32 is the requirement of the weight
