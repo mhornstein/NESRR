379cbe396b7f6ecf6faa2e2f480f8385f0363d5f
maor hornstein <mor.h100@gmail.com>
2023-08-31 15:50:11 +0300

    Adding the option to output testset prediction for each experiment
    Note: this is added as an option, as there are > ???, which is quite a lot of files to keep.

--- 6 baslines models/experimenter.py
+++ 6 baslines models/experimenter.py
@@ -34,18 +34,15 @@ def get_heuristics_scores(s):
 if __name__ == '__main__':
     start_time = time.time()
 
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 4:
         raise ValueError(f'Not enough arguments. Arguments given: {sys.argv[1:]}')
 
     input_file = sys.argv[1] # e.g. '../data/dummy/dummy_data.csv' or '../data/data.csv'
-    result_dir = sys.argv[2] # e.g. 'results'
+    log_test_classification = eval(sys.argv[2]) # should be either True or False
+    result_dir = sys.argv[3] # e.g. 'results'
 
     if not os.path.exists(result_dir):
         os.makedirs(result_dir)
-
-    test_classification_report_dir = f'{result_dir}\\test_classification_reports'
-    if not os.path.exists(test_classification_report_dir):
-        os.makedirs(test_classification_report_dir)
 
     experiment_log_file_path = f'{result_dir}\\experiments_logs.csv'
     exp_index = init_experiment_log_file(experiment_log_file_path, CONFIG_HEADER, RESULTS_HEADER)
@@ -77,6 +74,10 @@ if __name__ == '__main__':
                     'heuristic_threshold': heuristic_threshold
                 }
 
+                output_dir = f'{result_dir}\\{exp_index}'
+                if not os.path.exists(output_dir):
+                    os.makedirs(output_dir)
+
                 experiment_settings_str = ','.join([f'{key}={value}' for key, value in experiment_settings.items()])
                 print('running: ' + experiment_settings_str)
 
@@ -88,8 +89,23 @@ if __name__ == '__main__':
                                       'val_acc': accuracy_score(y_val, val_predictions),
                                       'test_acc': accuracy_score(y_test, test_predictions)}
 
+                if log_test_classification == True:
+                    sent_ids = X_test.index
+                    out_df = pd.DataFrame({'sent_ids': sent_ids,
+                                          'target_label': y_test,
+                                          'predicted_label': test_predictions,
+                                          'predicted_score': X_test})
+                    out_df = out_df.set_index('sent_ids', drop=True)
+                    out_df.index.name = None  # remove index column name
+
+                    test_data = df.loc[sent_ids]
+                    test_data = test_data[['label1', 'label2', 'ent1', 'ent2', 'masked_sent']]
+
+                    out_df = pd.concat([out_df, test_data], axis=1)
+                    out_df.to_csv(f'{output_dir}\\test_predictions_results.csv', index=True)
+
                 test_classification_report = classification_report(y_test, test_predictions, zero_division=1)
-                with open(f'{test_classification_report_dir}\\{exp_index}.txt', 'w') as file:
+                with open(f'{output_dir}\\test_report.txt', 'w') as file:
                     file.write(f'Test classification report:\n')
                     file.write(test_classification_report)
 
